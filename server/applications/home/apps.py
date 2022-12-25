9f8bddee48b70e6b5fe551cc4edccf196d878210
pradip pandey <pradip.meerako@gmail.com>
2022-12-26 00:47:35 +0545

    hello world

--- server/applications/home/apps.py
+++ server/applications/home/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
-
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home'
+    name = 'applications.home'
     verbose_name = 'Home'
--- server/server/urls.py
+++ server/server/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('applications.home.urls')),
+    # path('',include('applications.home.urls')),
     path('admin/', admin.site.urls),
 ]
